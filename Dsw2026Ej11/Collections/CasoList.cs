b61dd5cf1ce64d4636e298f0a5c190e30fe66b6d
Zambrano Agustina Nahir <aguszambrano01@gmail.com>
2026-05-22 17:39:26 -0300

    finaliza la resolucion de todos los casos

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -7,5 +9,46 @@
 //Incluir un método para eliminar un alumno (debe recibir un alumno)
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
-{
+{ 
+    private List<Alumno> alumnos = new List<Alumno>();
+
+    public void AgregarAlumno(Alumno alumno)
+    {
+        alumnos.Add(alumno);
+    }
+
+    public List<Alumno> RetornarLista()
+    {
+        return alumnos;
+    }
+
+    public Alumno BuscarAlumno(string nombre)
+    {
+        foreach (Alumno al in alumnos) 
+        {
+            if (al.Nombre.ToLower() == nombre.ToLower())
+            {
+                return al;
+            }
+        
+        }
+        return null;
+    }
+
+    public bool EliminarAlumno(Alumno alumno)
+    {
+        return alumnos.Remove(alumno);
+    }
+
+    public bool EliminarAlumnoPosicion(int posicion)
+    {
+        if (posicion >= 0 && posicion < alumnos.Count)
+        {
+            alumnos.RemoveAt(posicion);
+            return true;
+        }
+        return false;
+    }
 }
+
+
--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,4 +1,8 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
@@ -11,7 +15,59 @@ internal class Ejemplos
     //Eliminar el primer elemento de la lista y listar por consola los alumnos
     public static void EjemploList()
     {
+        CasoList casoList = new CasoList();
 
+        Alumno al1 = new Alumno( 1,"Juan", 8.5);
+        Alumno al2 = new Alumno( 2,"Pepe", 9);
+        Alumno al3 = new Alumno(3, "Roberto", 7.5);
+
+        casoList.AgregarAlumno(al1);
+        casoList.AgregarAlumno(al2);
+        casoList.AgregarAlumno(al3);
+
+        Console.WriteLine("Listado de alumnos:");
+        foreach(Alumno al in casoList.RetornarLista())
+        {
+            Console.WriteLine(al.ToString());
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Buscar alumno por nombre:");
+        Alumno alumnoEncontrado = casoList.BuscarAlumno("Pepe");
+
+        if (alumnoEncontrado != null)
+        {
+            Console.WriteLine($"Alumno encontrado: {alumnoEncontrado}, Id: {alumnoEncontrado.Id}, promedio: {alumnoEncontrado.Promedio}");
+        }
+        else
+        {
+            Console.WriteLine("No existe");
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Buscar alumno por nombre que no existe:");
+        Alumno alumnoNoEncontrado = casoList.BuscarAlumno("Agustina");
+
+        if (alumnoNoEncontrado != null)
+        {
+            Console.WriteLine($"Encontrado: {alumnoNoEncontrado.Nombre}");
+        }
+        else { Console.WriteLine("No existe"); }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Eliminar alumno por nombre:");
+        casoList.EliminarAlumno(al3);
+        Console.WriteLine("Listado de alumnos después de eliminar alumno:");
+        casoList.RetornarLista();
+        Console.WriteLine(); 
+
+
+        Console.WriteLine("Eliminar el primer elemento de la lista:");
+        casoList.EliminarAlumnoPosicion(0);
+        Console.WriteLine("Listado de alumnos después de eliminar el primer alumno:");
+        casoList.RetornarLista();
     }
 
     //Agregar 3 alumnos al diccionario
@@ -21,12 +77,134 @@ internal class Ejemplos
     //Eliminar un alumno por clave y listar por consola los alumnos
     public static void EjemploDictionary()
     {
+        CasoDictionary casoDictionary = new CasoDictionary();
+
+
+        Alumno al1 = new Alumno(  1,  "Juan", 8.5);
+        Alumno al2 = new Alumno( 2,  "Pepe", 9);
+        Alumno al3 = new Alumno(  3,"Roberto", 7.5);
+
+        casoDictionary.AgregarAlumno(al1.Id, al1);
+        casoDictionary.AgregarAlumno(al2.Id, al2);
+        casoDictionary.AgregarAlumno(al3.Id, al3);
+
+        Console.WriteLine("Listado de alumnos:");
+        Dictionary<int, Alumno> alumnos = casoDictionary.RetornarDiccionario();
+
+        foreach(KeyValuePair<int, Alumno> alumno in alumnos)
+        {
+            Console.WriteLine($"Legajo: {alumno.Key} -> {alumno.Value.ToString()}");
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Buscar alumno por clave:");
+        Alumno alumnoEncontrado = casoDictionary.BuscarAlumno(2);
+
+        if(alumnoEncontrado != null)
+        {
+            Console.WriteLine($"Alumno encontrado: {alumnoEncontrado.Nombre}, Id: {alumnoEncontrado.Id}");
+        }
+        else
+        {
+            Console.WriteLine("No existe");
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Buscar alumno por clave que no existe:");
+        Alumno alumnoNoEncontrado = casoDictionary.BuscarAlumno(4);
+
+        if(alumnoNoEncontrado != null)
+        {
+            Console.WriteLine($"Alumno encontrado: {alumnoNoEncontrado.Nombre}, Id: {alumnoNoEncontrado.Id}");
+        }
+        else
+        {
+            Console.WriteLine("No existe");
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Eliminar alumno por clave:");
+        casoDictionary.EliminarAlumno(2);
+
+        foreach (KeyValuePair<int, Alumno> alumno in casoDictionary.RetornarDiccionario())
+        {
+            Console.WriteLine($"Legajo: {alumno.Key}, Nombre: {alumno.Value}");
+        }
 
     }
 
     //Realizar una llamada a cada método definido en CasoLinq y mostar por consola según corresponda
     public static void EjemploLinq()
     {
+        Console.WriteLine("Ejemplo LINQ:");
+
+        CasoLinq casoLinq = new CasoLinq(Libro.CrearLista());
+
+        
+
+        Libro primero = casoLinq.GetPrimero();
+        Console.WriteLine($"Primer libro: {primero?.Titulo}, Precio: {primero?.Precio:C}");
+
+        Libro ultimo = casoLinq.GetUltimo();
+        Console.WriteLine($"Último libro: {ultimo?.Titulo}, Precio: {ultimo?.Precio:C}");
+
+        decimal totalPrecios = casoLinq.GetTotalPrecios();
+        Console.WriteLine($"Total de precios de los libros: {totalPrecios:C}");
+
+        decimal promedioPrecios = casoLinq.GetPromedioPrecios();
+        Console.WriteLine($"Promedio de precios de los libros: {promedioPrecios:C}");
+        Console.WriteLine();
+
+        Console.WriteLine("Libros con Id mayor a 15");
+        List<Libro> librosById = casoLinq.GetListById();
+        foreach (Libro libro in librosById)
+        {
+            Console.WriteLine($"Id: {libro.Id}, Título: {libro.Titulo}, Precio: {libro.Precio:C}");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Lista de libros con precio en formato moneda:");
+        List<string> librosFormateados = casoLinq.GetLibros();
+        foreach (string libro in librosFormateados)
+        {
+            Console.WriteLine(libro);
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Libro con mayor precio:");
+        Libro libroMasCaro = casoLinq.GetMayorPrecio();
+        Console.WriteLine($"Título: {libroMasCaro?.Titulo}, Precio: {libroMasCaro?.Precio:C}");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Libro con menor precio:");
+        Libro libroMasBarato = casoLinq.GetMenorPrecio();
+        Console.WriteLine($"Título: {libroMasBarato?.Titulo}, Precio: {libroMasBarato?.Precio:C}");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Libros con precio mayor al promedio");
+        List<Libro> librosMayorPromedio = casoLinq.GetMayorPromedio();
+        foreach (Libro libro in librosMayorPromedio)
+        {
+            Console.WriteLine($"Título: {libro.Titulo}, Precio: {libro.Precio:C}");
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("Libros ordenados por precio de mayor a menor:");
+        List<Libro> librosOrdenados = casoLinq.GetLibrosOrdenados();
+        foreach (Libro libro in librosOrdenados)
+        {
+            Console.WriteLine($"Título: {libro.Titulo}, Precio: {libro.Precio:C}");
+        }
+
+
+
+
 
     }
 }
